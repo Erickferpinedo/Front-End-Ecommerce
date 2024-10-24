141b26a42bf9b7ff89bf94eacbf627d179407242
Edwin Cely <edwincely6@gmail.com>
2024-10-23 22:49:11 -0600

    header nav hamburguer menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
@@ -6,8 +6,50 @@ import { RouterLinkWithHref } from '@angular/router';
   standalone: true,
   imports: [RouterLinkWithHref],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+    const nav = document.querySelector('#nav');
+    const open = document.querySelector('#open');
+    const close = document.querySelector('#close');
+    const overlay = document.querySelector('#overlay ');
 
+    open?.addEventListener('click', () => {
+      nav?.classList.add('visible');
+      overlay?.classList.add('active');
+      console.log('se abrio pero no se aplico la clase');
+    });
+
+    close?.addEventListener('click', () => {
+      nav?.classList.remove('visible');
+      overlay?.classList.remove('active');
+      console.log('se cerro pero no se aplico la clase');
+    });
+
+    open?.addEventListener('click', function () {
+      document.querySelector('.nav-hamburguer')?.classList.add('menu-open');
+    });
+
+    close?.addEventListener('click', function () {
+      document.querySelector('.nav-hamburguer')?.classList.remove('menu-open');
+    });
+
+    overlay?.addEventListener('click', () => {
+      nav?.classList.remove('visible');
+      overlay.classList.remove('active');
+    });
+  }
+
+  @ViewChild('dropdownContent', { static: false }) dropdownContent!: ElementRef;
+
+  showMenu() {
+    this.dropdownContent.nativeElement.style.display = 'block';
+  }
+
+  hideMenu(event: MouseEvent) {
+    if (!this.dropdownContent.nativeElement.contains(event.relatedTarget)) {
+      this.dropdownContent.nativeElement.style.display = 'none';
+    }
+  }
 }
