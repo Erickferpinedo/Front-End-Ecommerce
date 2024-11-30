db3f58bfd8418ccb2ab24d43f5211c6d7532ce5d
Edwin Cely <edwincely6@gmail.com>
2024-11-29 18:44:01 -0600

    add banner home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductoDestacadoComponent } from '../../components/producto-destacado/producto-destacado.component';
 import { SectionCategoriesComponent } from "../../components/section-categories/section-categories.component";
+import { BannerComponent } from "../../components/banner/banner.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ProductoDestacadoComponent, SectionCategoriesComponent],
+  imports: [CommonModule, ProductoDestacadoComponent, SectionCategoriesComponent, BannerComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
